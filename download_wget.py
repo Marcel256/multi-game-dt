985d8a3243c50a4f6e9efc26ffe8018105a2b6ed
Marcel256 <marcel.brunnbauer@gmx.at>
2023-03-08 10:54:57 +0100

    add wget download script

---
+++ download_wget.py
@@ -0,0 +1,35 @@
+import os
+url = 'https://www.someurl.com'
+output_file = ""
+
+
+
+
+
+games = [
+'Skiing',
+'Breakout',
+'DemonAttack',
+'SpaceInvaders',
+'Assault',
+]
+file_types = ['observation', 'action', 'reward', 'terminal']
+file_range = range(41, 43)
+
+def get_file_name(type, file_number):
+    return '$store$_{}_ckpt.{}.gz'.format(type, file_number)
+
+url_pattern = 'http://storage.googleapis.com/atari-replay-datasets/dqn/{}/1/replay_logs/{}'
+download_dir = 'data/download'
+
+for game in games:
+    game_dir = os.path.join(download_dir, game)
+    if not os.path.exists(game_dir):
+        os.makedirs(game_dir)
+    for i in file_range:
+        for file_type in file_types:
+            file_name = get_file_name(file_type, i)
+            path = url_pattern.format(game, file_name)
+            print('Downloading ', path)
+            out_file = os.path.join(game_dir, file_name)
+            os.system(f"""wget -O "{out_file}" "{path}""")
